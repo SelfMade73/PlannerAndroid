98f5670244221dab6cf8ef8e3eed1f4154e31c3a
Алексей Агафонов <sun04sun04@gmail.com>
2020-05-25 09:57:58 +0300

    Добавил bottom sheet прикрепил к нему fab сделал анимацию сжатия при изменении bottom sheet

--- app/src/main/java/com/example/myapplication/MainDashBoard.java
+++ app/src/main/java/com/example/myapplication/MainDashBoard.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainDashBoard extends AppCompatActivity {
+    private LinearLayout bottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dash_board);
+
+        Window window = getWindow();
+        //transparent status bar
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        fab = findViewById(R.id.fab_new_note);
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+              @Override
+              public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+              }
+
+              @Override
+              public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    fab.animate().scaleX(1 - slideOffset).scaleY(1 - slideOffset).setDuration(0).start();
+              }
+        }
+
+
+        );
     }
 }
